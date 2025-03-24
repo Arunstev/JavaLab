2246b596844d85f55a994155041f0d6f98aa8501
Arunstev <121638809+Arunstev@users.noreply.github.com>
2025-03-24 20:27:41 +0000

    ...x

--- MyJavaLab/src/main/java/com/stev/project/Chapter1.java
+++ MyJavaLab/src/main/java/com/stev/project/Chapter1.java
@@ -1,8 +1,15 @@
 package com.stev.project;
-
+import java.util.Scanner;
 
 public class Chapter1{
+    
     public static void main(String[] args) {
-        System.out.println("Hello"); 
+        Scanner sc = new scanner(System.in);
+       
+        System.out.println("Hey! I'm Java, What your name!: ");
+        String name = sc.nextLine;
+
+        System.out.println(" your age!");
+        int age = sc.nextInt();
     }
 }
